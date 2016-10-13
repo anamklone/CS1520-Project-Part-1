9f7669ce5dcb07b021edda04cf05768a864faf74
anamklone <amk226@pitt.edu>
2016-10-13 02:49:23 -0400

    Text to confirm contact form submission

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 (function($) {
   $('#contactForm').on('submit', function(e) {
     e.preventDefault();
-    console.log('Nope');
+    var valid = true;
     if ($('input[name=name]').val() == null || $('input[name=name]').val() == '') {
+      valid = false;
       $('#contactForm').before('<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>Error!</strong> User input <em>name</em> is not valid.</div>');
     }
     if ($('input[name=email]').val() == null || $('input[name=email]').val() == '') {
+      valid = false;
       $('#contactForm').before('<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>Error!</strong> User input <em>email</em> is not valid.</div>');
+    }
+    if (valid) {
+      $('#contact-form').html('<h2 style="text-align:center">Thank you for your message! :D</h2>');
     }
   });
 })(jQuery);
